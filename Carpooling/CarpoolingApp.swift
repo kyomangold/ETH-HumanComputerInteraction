cb801453e6d19f28f02fbbf724c7960b57e92099
Kyo Mangold <kyo.mangold@bluewin.ch>
2021-11-16 14:50:49 +0100

    Initial Commit

---
+++ Carpooling/CarpoolingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarpoolingApp.swift
+//  Carpooling
+//
+//  Created by Kyo Mangold on 16.11.21.
+//
+
+import SwiftUI
+
+@main
+struct CarpoolingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carpooling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Carpooling
+//
+//  Created by Kyo Mangold on 16.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
